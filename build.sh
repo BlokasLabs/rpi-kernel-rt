15a35e9c66f21b7f8c261773215e147ace9da9b1
Giedrius <giedrius@blokas.io>
2019-08-21 18:32:52 +0300

    Added rt kernel building for RPi4

--- build.sh
+++ build.sh
@@ -2,30 +2,31 @@
 
 mkdir -p build/boot/overlays
 
-git clone --depth 1 --branch rpi-4.14.y-rt https://github.com/raspberrypi/linux
+git clone --depth 1 --branch rpi-4.19.y-rt https://github.com/raspberrypi/linux
 cd linux
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 export INSTALL_MOD_PATH=../build
 
+# Arg1: kernel file name, Arg2: config to use
+build_kernel() {
+	local KERNEL=$1
+	make mrproper
+	make $2
+	make zImage modules dtbs -j8
+	make modules_install
+	cp Module.symvers ../build/boot/${KERNEL}_Module.symvers
+	cp arch/arm/boot/zImage ../build/boot/$KERNEL.img
+	cp arch/arm/boot/dts/*.dtb ../build/boot/
+	cp arch/arm/boot/dts/overlays/*.dtb* ../build/boot/overlays/
+	cp arch/arm/boot/dts/overlays/README ../build/boot/overlays/
+}
+
 # Build kernel for Pi 1, Pi 0, Pi 0 W and Compute Module
-export KERNEL=kernel
-make bcmrpi_defconfig
-make zImage modules dtbs -j8
-make modules_install
-cp arch/arm/boot/zImage ../build/boot/$KERNEL.img
-cp arch/arm/boot/dts/*.dtb ../build/boot/
-cp arch/arm/boot/dts/overlays/*.dtb* ../build/boot/overlays/
-cp arch/arm/boot/dts/overlays/README ../build/boot/overlays/
+build_kernel kernel bcmrpi_defconfig
 
 # Build kernel for Pi 2, Pi 3, Pi 3+ and Compute Module 3
-export KERNEL=kernel7
-make mrproper
-make bcm2709_defconfig
-make zImage modules dtbs -j8
-make modules_install
-cp arch/arm/boot/zImage ../build/boot/$KERNEL.img
-cp arch/arm/boot/dts/*.dtb ../build/boot/
-cp arch/arm/boot/dts/overlays/*.dtb* ../build/boot/overlays/
-cp arch/arm/boot/dts/overlays/README ../build/boot/overlays/
+build_kernel kernel7 bcm2709_defconfig
 
+# Build kerenl for Pi 4
+build_kernel kernel7l bcm2711_defconfig
