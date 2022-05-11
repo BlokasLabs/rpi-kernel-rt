fbcd0c7b8fe6a1cae93390d2692acac972d701e5
Giedrius <giedrius@blokas.io>
2022-05-11 15:04:15 +0300

    Updated build.sh for 5.15 rt kernel

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build/boot/overlays
 
-git clone --depth 1 --branch rpi-4.19.y-rt https://github.com/BlokasLabs/rpi-linux-rt linux
+git clone --depth 1 --branch blokas-rpi-5.15.y-rt https://github.com/BlokasLabs/rpi-linux-rt linux
 cd linux
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
@@ -19,24 +19,32 @@ build_kernel() {
 	# git_hash
 	make mrproper
 	make $2
-	make zImage modules dtbs -j8
+	make $3 modules dtbs -j8
 	make modules_install
 	cp Module.symvers ../build/boot/${MODULE_SYMVERS}
 	cp System.map ../build/boot/${SYSTEM_MAP}
-	cp arch/arm/boot/zImage ../build/boot/${KERNEL}.img
-	cp arch/arm/boot/dts/*.dtb ../build/boot/
-	cp arch/arm/boot/dts/overlays/*.dtb* ../build/boot/overlays/
-	cp arch/arm/boot/dts/overlays/README ../build/boot/overlays/
+	cp arch/${ARCH}/boot/$3 ../build/boot/${KERNEL}.img
+	if [ "$ARCH" = "arm" ]; then
+		cp arch/${ARCH}/boot/dts/*.dtb ../build/boot/
+	else
+		cp arch/${ARCH}/boot/dts/broadcom/*.dtb ../build/boot/
+	fi
+	cp arch/${ARCH}/boot/dts/overlays/*.dtb* ../build/boot/overlays/
+	cp arch/${ARCH}/boot/dts/overlays/README ../build/boot/overlays/
 }
 
 # Build kernel for Pi 1, Pi 0, Pi 0 W and Compute Module
-build_kernel "" bcmrpi_defconfig
+build_kernel "" bcmrpi_defconfig zImage
 
 # Build kernel for Pi 2, Pi 3, Pi 3+ and Compute Module 3
-build_kernel "7" bcm2709_defconfig
+build_kernel "7" bcm2709_defconfig zImage
 
 # Build kerenl for Pi 4
-build_kernel "7l" bcm2711_defconfig
+build_kernel "7l" bcm2711_defconfig zImage
+
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+build_kernel "8" bcm2711_defconfig Image
 
 # Run depmod for all kernels and unlink build and source
 find "../build/lib/modules" -mindepth 1 -maxdepth 1 -type d | while read DIR; do
